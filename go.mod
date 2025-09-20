748a9be44e8c7df7b3245d484c462a129f3fc390
drofylla <drofylla@gmail.com>
2025-09-20 10:07:33 +0800

    serve

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module project/afcb
 go 1.24.4
 
 require github.com/sqids/sqids-go v0.4.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,46 +1,95 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+var contacts Contacts
+
+func getContacts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application.json")
+	json.NewEncoder(w).Encode(contacts)
+}
+
+func addContact(w http.ResponseWriter, r *http.Request) {
+	var c Contact
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func deleteContact(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	if err := contacts.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateContact(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var updates map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&updates); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := contacts.UpdateContact(id, updates); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	json.NewEncoder().Encode("updated")
+}
+
 func main() {
-	var contacts Contacts
 
-	contacts.New("Friend", "Orm", "Korn", "ok@kshhh.com", "012-3456-789")
-	contacts.New("Family", "Zal", "Ahm", "za@zba.com", "133-0133-013")
-	contacts.New("Work", "Tay", "Swi", "ts@tas.com", "198-9198-919")
+	router := mux.NewRouter()
 
-	fmt.Println("Contacts List")
-	for _, c := range contacts {
-		fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
-	}
+	router.HandleFunc("/contacts", getContacts).Methods("GET")
+	router.HandleFunc("/contacts", addContact).Methods("POST")
+	router.HandleFunc("/contacts/{id}", deleteContact).Methods("DELETE")
+	router.HandleFunc("/contacts/{id}", updateContact).Methods("PUT")
 
-	deleteID := contacts[1].ID
-	fmt.Printf("\nDeleting ID %s..\n", deleteID)
-	if err := contacts.Delete(deleteID); err != nil {
-		fmt.Println("Error:", err)
-	}
+	http.ListenAndServe(":1330", router)
 
-	fmt.Println("\nUpdated Contacts List")
-	for _, c := range contacts {
-		fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
-	}
+	// contacts.New("Friend", "Orm", "Korn", "ok@kshhh.com", "012-3456-789")
+	// contacts.New("Family", "Zal", "Ahm", "za@zba.com", "133-0133-013")
+	// contacts.New("Work", "Tay", "Swi", "ts@tas.com", "198-9198-919")
 
-	updateID := contacts[1].ID
-	fmt.Printf("\nUpdating Taylor's name...\n")
+	// fmt.Println("Contacts List")
+	// for _, c := range contacts {
+	// 	fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
+	// }
 
-	err := contacts.UpdateContact(updateID, map[string]string{
-		"First Name": "Taylor",
-		"Last Name":  "Swift",
-	})
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	// deleteID := contacts[1].ID
+	// fmt.Printf("\nDeleting ID %s..\n", deleteID)
+	// if err := contacts.Delete(deleteID); err != nil {
+	// 	fmt.Println("Error:", err)
+	// }
 
-	fmt.Println("\nUpdated Contacts List")
-	for _, c := range contacts {
-		fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
-	}
+	// fmt.Println("\nUpdated Contacts List")
+	// for _, c := range contacts {
+	// 	fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
+	// }
+
+	// updateID := contacts[1].ID
+	// fmt.Printf("\nUpdating Taylor's name...\n")
+
+	// err := contacts.UpdateContact(updateID, map[string]string{
+	// 	"First Name": "Taylor",
+	// 	"Last Name":  "Swift",
+	// })
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// }
+
+	// fmt.Println("\nUpdated Contacts List")
+	// for _, c := range contacts {
+	// 	fmt.Printf("ID: %s | Name: %s %s | Email: %s | Phone: %s\n", c.ID, c.FirstName, c.LastName, c.Email, c.Phone)
+	// }
 
 }
